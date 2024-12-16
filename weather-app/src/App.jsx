d36942aee2534cf6594af438ce1e90a608b1f5b4
Hadent Dang <hadentdang@gmail.com>
2024-12-16 12:32:06 -0600

    initial commit

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-white min-w-96 max-w-full min-h-96 max-h-full w-3/4 h-max mx-auto my-12 rounded-xl border-2 border-black">
+    </div>
+  )
+}
+
+export default App
